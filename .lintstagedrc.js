d0e31d0eea681ef09910dd3b27970f7435d18b11
devrazzak <developerrazzak@gmail.com>
2025-02-15 12:01:01 +0600

    fix issue

---
+++ .lintstagedrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+    '*.{js,jsx,ts,tsx}': ['eslint --cache --fix', 'prettier --write'],
+    '*.{json,css,md}': ['prettier --write'],
+};
--- i18n.ts
+++ i18n.ts
@@ -5,15 +5,15 @@ export const locales: string[] = ["en", "es", "bn"];
 export const defaultLocale: string = "en";
 
 export default getRequestConfig(async ({ locale }: { locale?: string }) => {
-  if (!locale || !locales.includes(locale)) {
-    notFound();
-  },
+    if (!locale || !locales.includes(locale)) {
+        notFound();
+    }
 
-  try {
-    const messages = (await import(`./messages/${locale}.json`)).default;
-    return { messages };
-  } catch (error) {
-    console.error(`Error loading messages for locale: ${locale}`, error);
-    notFound(); // Handle missing locale files gracefully
-  }
+    try {
+        const messages = (await import(`./messages/${locale}.json`)).default;
+        return { messages };
+    } catch (error) {
+        console.error(`Error loading messages for locale: ${locale}`, error);
+        notFound(); // Handle missing locale files gracefully
+    }
 });
